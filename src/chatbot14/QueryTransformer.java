190d51499dea9823445f46936b254af6bbca6adb
Nicholas <46433505+NicholasReis@users.noreply.github.com>
2020-02-23 11:42:05 -0800

    Created this file
    It's a placeholder for now. This is where we can transform the sentences. (Whatever that means.)

---
+++ src/chatbot14/QueryTransformer.java
@@ -0,0 +1,15 @@
+public class QueryTransformer
+{
+    /*
+     * Param is placeholder for when we actually need to transform query
+     * This is where we will take a String array as input, transform it,
+     * and return it as a String array.
+     */
+    public String[] transform(String[] x)
+    {
+        //This is just a temporary array so it compiles
+        String[] transformedQuery = {"G", "I"};
+        
+        return transformedQuery;
+    }
+}
